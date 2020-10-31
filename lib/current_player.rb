503d86966e8e3f12900181d414502ad79bdfd820
Muhammad mahdi <danyalm50@gmail.com>
2020-10-31 18:34:40 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,20 @@
+def turn_count(board)
+  counter = 0
+
+  board.each do |turn|
+    if turn == "X" || turn == "O"
+      counter += 1
+      puts "#{counter}"
+    end
+  end
+
+  counter
+end
+
+def current_player(board)
+  turn_count(board)
+  if 4 % 2 == 0
+    return "X"
+  else "O"
+  end
+  end
